74fda710ddc8a053d08068b8685e920c5af5fafa
Chinmay Ghodake <chinmay.ghodake@MacBook-Pro.local>
2026-07-20 14:25:50 +0530

    Ruby blanks assignment for enumerables and code blocks

--- ruby_blanks.rb
+++ ruby_blanks.rb
@@ -1,15 +1,23 @@
 puts "The words you provide to us will be used to create a funny sentence"\
 
-print"Give me a verbs:"
-verb=gets.chomp
+# print"Give me a verbs:"
+# verb=gets.chomp
 
-print"Give me an adjective:"
-adj1=gets.chomp
+# print"Give me an adjective:"
+# adj1=gets.chomp
 
-print"Give me another adjective:"
-adj2=gets.chomp
+# print"Give me another adjective:"
+# adj2=gets.chomp
 
-print"Give me Noun:"
-noun=gets.chomp
+# print"Give me Noun:"
+# noun=gets.chomp
+blanks=["verb","adjective","adjective","noun"]
+vowels=["a","e","i","o","u"]
 
-puts "I have decided to #{verb} a #{adj1}party for my #{adj2} #{noun}."
+answers=blanks.map do |request|
+    article=vowels.include?(request[0])? "an" : "a"
+    print"Give me #{article} #{request}:"
+    response=gets.chomp
+end
+
+puts "I have decided to #{answers[0]} a #{answers[1]} party for my #{answers[2]} #{answers[3]}."
